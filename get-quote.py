13b719e4b749bc2815b058101acea3db7ecaf13d
babax85 <86411975+babax85@users.noreply.github.com>
2021-06-29 13:59:47 +0200

    Added a quote

--- get-quote.py
+++ get-quote.py
@@ -4,12 +4,24 @@ def primary():
 
  f = open("quotes.txt")
  quotes = f.readlines()
+ #f.write("Don't delete existing data \n")
  f.close()
-  
- last = 13
- rnd = random.randint(0, last) 
- print(quotes[rnd])
-
+ 
+ #f = open("quotes.txt", "r")
+ 
+ last = len(quotes) -1
+ rnd1 = random.randint(0, last)
+ rnd2 = random.randint(0, last)
+ 
+ 
+ print(quotes[rnd1], quotes[rnd2], sep="\n")
+ 
+ #print(quotes[rnd1], end =" "),
+ #print(quotes[rnd2])
+ 
+ #mylist = [quotes[rnd1], quotes[rnd2]]
+ #for i in mylist:
+ #  print (i, end=" ")
 
 if __name__== "__main__":
  primary()
